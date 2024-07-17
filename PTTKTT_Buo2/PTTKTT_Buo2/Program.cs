3aec43ac0b3ebf2360011e1236d0082f9cc14fc2
Yu211203 <thuxuyen211@gmail.com>
2024-07-17 09:55:32 +0700

    Update PTTKTT Buo2 file and add Binarysearch video

--- PTTKTT_Buo2/PTTKTT_Buo2/Program.cs
+++ PTTKTT_Buo2/PTTKTT_Buo2/Program.cs
@@ -8,8 +8,39 @@ namespace PTTKTT_Buo2
 {
     class Program
     {
+        public static int Binary_search(int[] A, int n, int key)
+        { 
+            int left = 0; 
+            int right = n-1; 
+            while (left <= right)
+          {
+            int mid = (left + right)/2; 
+                if (A[mid] == key)
+            return mid;
+                if (key < A[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+        }
+            return -1; 
+        }
         static void Main(string[] args)
         {
+            int[] A = { 1, 4, 5, 6, 12, 2, 23, 4, 52, 16 };
+            
+            Console.Write("Nhap so n: ");
+            //int n = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+
+            int kq = Binary_search(A, 10, x);
+            if(kq != -1)
+            {
+                Console.WriteLine("Tai vi tri " + kq);
+            }else
+            {
+                Console.WriteLine("Khong tim thay");
+            }
+            Console.ReadKey();
         }
     }
 }
